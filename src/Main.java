2c2c34f0437abc222f97658c114400c8ad95e6bf
LuisCanuto <1251193@isep.ipp.pt>
2025-12-13 19:50:46 +0000

    Adiciona modulo que verifica se matriz é estável, retorna em boleean

--- src/Main.java
+++ src/Main.java
@@ -3,4 +3,26 @@ import java.io.FileNotFoundException;
 import java.util.Scanner;
 public class Main{
 
+    public static int Zc = 4;
+
+
+       //________________________________________________
+      //                                                \\
+     //      Modulo Checar Estabilidade de matrizes      \\
+    //                                                    \\
+    public boolean ChecadorDeEstabilidadeDeMatrizes ( int [][] matriz){
+
+        //Ciclos "enchented for" percorrem todos os termos da matriz
+        for ( int [] linha : matriz ){
+            for ( int termo : linha ){
+                //Termo selecionado
+                if ( termo >= Zc){
+                    //Se um termo selecionado for maior ou igual a Zc, a Matriz não é estável
+                    return false;
+                }
+            }
+        }
+        //Se depois dos ciclos "for" nenum termo for maior ou igual a Zc, a matriz é estável
+        return true;
+    }
 }
