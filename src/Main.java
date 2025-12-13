85c8ba21b48f51cc95dd3c56c78f4e3fc6353f83
LuisCanuto <1251193@isep.ipp.pt>
2025-12-13 17:55:47 +0000

    Modulo que estabiliza Matrizes + Zc definida como váriavel global

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,52 @@
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
+
+
 public class Main{
 
+    public static int Zc = 4;
+
+    //||||||||||||||||||||||||||||||||||||||||||||\\
+   //                                              \\
+  //           Modulo Operação Toppin               \\
+ //                                                  \\
+    public static int[][] toppingMatrizes(int[][] matriz) {
+
+        int numeroIndiceMáximo = matriz.length - 1;   //É a váriavel que armazena o maior Índice da coluna e da linha
+        int nDaMatriz = matriz[0].length;             //É a váriavel que armazena a quantidade de termos em cada coluna e linha
+        int termoAnulado = 0;                         //É uma váriavel usada para anular o termo selecionado no "for" que seja maior ou igual a Zc (número crítico)
+
+        //Ciclo dos termos da Matriz
+        for (int i = 0; i < nDaMatriz; i++) {
+            for (int j = 0; j < nDaMatriz; j++) {
+                //O topping é apenas aplicado se o termo selecionado for maior ou igual ao Zc (nnúmero crítico da rede)
+                if (matriz[i][j] >= Zc) {
+                    //O termo é anulado
+                    matriz[i][j] = termoAnulado;
+
+                    //Certas áreas da matriz possuem propriedades genéricas quando sofrem toppin dependendo da sua posição em relação a bordas
+                    //(Operações abaixo:)
+                    if ( i > 0 ) {
+
+                        matriz[i - 1][j] ++;
+                    }
+                    if ( j > 0 ) {
+
+                        matriz[i][j - 1] ++;
+                    }
+                    if ( i < numeroIndiceMáximo) {
+
+                        matriz[i + 1][j] ++;
+                    }
+                    if ( j < numeroIndiceMáximo) {
+
+                        matriz[i][j + 1] ++;
+                    }
+                }
+            }
+        }
+        //A matriz é retornada depois de sofrer toppin
+        return matriz;
+    }
 }
