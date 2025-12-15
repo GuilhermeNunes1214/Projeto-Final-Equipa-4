d5f4178a441b8b54b9a9606b85324f0097684402
AbelSilva98 <1250687@isep.ipp.pt>
2025-12-15 11:14:19 +0000

    Criei um módulo para imprimir a matriz do input

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,13 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
 public class Main{
-
-}
+    public static void imprimirMatriz(int[][] matriz) {
+        System.out.println("Matriz Inicial:");
+        for (int i = 0; i < matriz.length; i++) {
+            System.out.print("|");
+            for (int j = 0; j < matriz[i].length; j++) {
+                System.out.printf(" %d |", matriz[i][j]);
+            }
+            System.out.println();
+        }
+    }
